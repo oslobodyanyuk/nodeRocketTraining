1ccedbd494cf7c861a21423fbc9b63f7cfc7f779
alexwizard@gmail.com <alexwizard@gmail.com>
2022-01-25 19:49:36 +0200

    Work with arrays & cycles -> class02

--- Class02/script02.js
+++ Class02/script02.js
@@ -1,1 +1,20 @@
-alert('ddd')
+function task01() {
+    let nums = [1, 5, 7, 4, 3]
+    console.log('nums->', nums)
+    let str = ['a', 'bb', 'ccc', 'dddd', 'eeeee']
+    console.log('str->', str)
+    let mas = [true, 1, 'true', 2, 3, false, NaN]
+    console.log('mas->', mas)
+}
+window.onload = function() {
+//  function task02() {
+    for (i=0;i<10;i++) {
+        let div_block = document.createElement('div')
+        let div_text = document.createTextNode('div '+i)
+        div_block.appendChild(div_text)
+        document.body.appendChild(div_block)
+    }
+
+
+}
+task01()
